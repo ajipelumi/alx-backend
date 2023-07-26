880dd9960606f39eb0e3e3237fc49de3e0fa9f86
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-26 23:27:52 +0100

    implemented most recently used algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -16,11 +16,16 @@ class MRUCache(BaseCaching):
         """ Add key/value pair to cache_data. """
         if key and item:
             self.cache_data[key] = item
+            # Update the mru_cache with the most recent key
             self.mru_cache[key] = datetime.now()
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            #  Sort the mru_cache by datetime
             mru_items = sorted(self.mru_cache.items(),
                                key=lambda x: x[1], reverse=True)
+            # Get the 2nd most recent key because the most recent key is the
+            # one that was just added
             mru_key = mru_items[1][0]
+            # Delete the 2nd most recent key from both dicts
             del self.mru_cache[mru_key]
             del self.cache_data[mru_key]
             print("DISCARD: {}".format(mru_key))
