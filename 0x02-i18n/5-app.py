b43be1374ecad5ffa7e9b966b387687fca3c7a0c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-02 21:47:04 +0100

    implemented mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" Basic Flask app with Babel setup. """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Dict
+
+
+app = Flask(__name__)
+
+
+class Config():
+    """ Babel config class. """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Get locale language code. """
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(login_as: int) -> Dict:
+    """ Get user from users. """
+    if login_as in users:
+        return users.get(login_as)
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """ Before request handler. """
+    login = request.args.get('login_as')
+    user = get_user(int(login))
+    if user:
+        g.user = user
+    else:
+        g.user = None
+
+
+@app.route('/')
+def index() -> str:
+    """ Basic Flask app. """
+    return render_template('5-index.html', user=g.user)
+
+
+if __name__ == '__main__':
+    app.run()
