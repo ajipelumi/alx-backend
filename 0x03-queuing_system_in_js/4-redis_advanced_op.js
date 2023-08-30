5c9e20d78683d152e1f3eaf25c6696936290b401
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-31 00:19:42 +0100

    implemented Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,28 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Connect to Redis
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+// Handle errors
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+});
+
+// Set Hash
+client.hset('HolbertonSchools', 'Portland', '50', redis.print);
+client.hset('HolbertonSchools', 'Seattle', '80', redis.print);
+client.hset('HolbertonSchools', 'New York', '20', redis.print);
+client.hset('HolbertonSchools', 'Bogota', '20', redis.print);
+client.hset('HolbertonSchools', 'Cali', '40', redis.print);
+client.hset('HolbertonSchools', 'Paris', '2', redis.print);
+
+// Get Hash
+client.hgetall('HolbertonSchools', (err, value) => {
+    console.log(value);
+});
